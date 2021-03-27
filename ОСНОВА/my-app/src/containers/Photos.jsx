150c5f42d9c1998f364b7a13804abfbe167a0b37
Maks <maxartem0419@gmail.com>
2021-03-28 00:40:42 +0300

    list

--- ОСНОВА/my-app/src/containers/Photos.jsx
+++ ОСНОВА/my-app/src/containers/Photos.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadPhotos } from "../actions/actions";
 import {unsplashGetListPhotos} from "../unsplash/unsplash";
@@ -10,12 +10,16 @@ import 'simplebar/dist/simplebar.min.css';
 
 function Photos (props) {
   useEffect(() => {
+    loadPhotos();
+  }, [])
+
+  function loadPhotos () {
     const page = localStorage.getItem("page");
     unsplashGetListPhotos(page).then((answer) => {
       props.loadPhotos(answer);
       localStorage.setItem("page", `${Number(page)+1}`)
     });
-  }, [props])
+  }
 
   return (
     <>
