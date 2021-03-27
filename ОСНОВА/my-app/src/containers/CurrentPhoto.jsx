91265cabb15446a07994c671ecea225ee0e6a7c4
Maks <maxartem0419@gmail.com>
2021-03-27 23:54:06 +0300

    test_2

--- ОСНОВА/my-app/src/containers/CurrentPhoto.jsx
+++ ОСНОВА/my-app/src/containers/CurrentPhoto.jsx
@@ -18,7 +18,7 @@ function CurrentPhoto (props) {
     document.body.style.overflowY = "hidden";
     getPhoto(props.match.params.id);
     document.body.style.overflowY = "auto";
-  }, [props.match.params.id])
+  }, [props.match.params.id, this.getPhoto])
 
   function getPhoto(id) {
     unsplashGetPhoto(id).then(photo => {
