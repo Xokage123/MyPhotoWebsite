362f54a99ed896541bf8441f88b27fb9f8f6b181
Maks <maxartem0419@gmail.com>
2021-04-05 18:57:33 +0300

    update Unsplash

--- ОСНОВА/my-app/src/unsplash/unsplash.js
+++ ОСНОВА/my-app/src/unsplash/unsplash.js
@@ -1,27 +1,24 @@
 import { createApi } from 'unsplash-js';
+import options from "../CONST";
 
 const unsplash = createApi({
-  accessKey: 'df7ICabs2U1sK_fvyU2BuzlCLv-eduqU51fMxIn6Hvs',
+    accessKey: options.access_key,
 });
 
-export async function unsplashGetPhoto (id) {
-  const answer = await unsplash.photos.get({photoId: id});
-  if (answer.status === 200) {
-    const photo = answer.response;
-    return photo;
-  }
+export async function unsplashGetPhoto(id) {
+    const answer = await unsplash.photos.get({ photoId: id });
+    if (answer.status === 200) {
+        const photo = answer.response;
+        return photo;
+    }
 }
 
-export async function unsplashGetListPhotos (page) {
-  const answer = await unsplash.photos.list({
-    page
-  });
-  if (answer.status === 200) {
-    const photoList = answer.response.results;
-    return photoList;
-  }
+export async function unsplashGetListPhotos(page) {
+    const answer = await unsplash.photos.list({
+        page
+    });
+    if (answer.status === 200) {
+        const photoList = answer.response.results;
+        return photoList;
+    }
 }
-
-
-
-
