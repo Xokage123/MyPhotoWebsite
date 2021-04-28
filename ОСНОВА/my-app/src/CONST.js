68809be514537c2181d0a9fa95593a0306718723
Maks <maxartem0419@gmail.com>
2021-04-28 13:04:58 +0300

    update

--- ОСНОВА/my-app/src/CONST.js
+++ ОСНОВА/my-app/src/CONST.js
@@ -1,7 +1,7 @@
 module.exports = {
     access_key: "df7ICabs2U1sK_fvyU2BuzlCLv-eduqU51fMxIn6Hvs",
     secret_key: "3cVeyZ3__ekbRdS5MuHkXN9yj4MFWCauLCVzE8hGhtA",
-    URI: 'urn:ietf:wg:oauth:2.0:oob',
+    URI: 'https://my-photo-website.vercel.app',
     response_type: "code",
     scope: "public+write_likes",
     // Каждый раз генерировать по новой
